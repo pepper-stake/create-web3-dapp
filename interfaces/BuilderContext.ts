74e2354ab6876c1bf85558c53e6ce4a6a88bb6f5
eversmile12 <iwasvitto@gmail.com>
2023-01-31 15:51:44 +0100

    refines folder structure

--- interfaces/BuilderContext.ts
+++ interfaces/BuilderContext.ts
@@ -2,7 +2,6 @@ import { SmartContractInfo } from "./SmartContractInfo.js";
 import { ERC721smartContractInfo } from "../interfaces/ERC721smartContractInfo.js";
 import { ERC20smartContractInfo } from "./ERC20smartContractInfo.js";
 import { DappInfo } from "./DappInfo.js";
-import { Multibar } from "../helpers/utils/progressBar.js";
 
 export interface BuilderContext {
 	contractInfo:
